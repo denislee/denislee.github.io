f997747049618242672b850864d6f2fd5e0b79d8
Denis Lee <denislee@gmail.com>
2026-07-05 00:10:01 -0300

    hn: digest for 2026-07-05 (30 stories)

--- hn/sw.js
+++ hn/sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'hn-digests-15a46626';
+const CACHE_NAME = 'hn-digests-37af8fbf';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './favicon.ico',
   './sw.js',
+  './2026-07-05.html',
+  './2026-07-05.epub',
+  './2026-07-05.md',
+  './2026-07-05.txt',
+  './2026-07-05.rsvp',
   './2026-07-04.html',
   './2026-07-04.epub',
   './2026-07-04.md',
